44fc3ac2c13d8353b2587a84e55e25e922a89485
Jose María Escribano <jescribanomora@gmail.com>
2022-08-30 19:21:28 +0200

    feature: add API endpoint

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::take(5)->get();
+
+        return $products;
+    }
 }
