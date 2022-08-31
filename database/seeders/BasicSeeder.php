6dad4e131085a5e1048569966660ce9990920224
Jose María Escribano <jescribanomora@gmail.com>
2022-08-31 13:20:18 +0200

    feature: create basic and stress seeders

---
+++ database/seeders/BasicSeeder.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BasicSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $current_date_time = Carbon::now()->toDateTimeString();
+
+        // Populate categories table
+        DB::table('categories')->insert([
+            [
+                "name" => "boots",
+                "created_at" => $current_date_time,
+                "updated_at" => $current_date_time
+            ],
+            [
+                "name" => "sandals",
+                "created_at" => $current_date_time,
+                "updated_at" => $current_date_time
+            ],
+            [
+                "name" => "sneakers",
+                "created_at" => $current_date_time,
+                "updated_at" => $current_date_time
+            ],
+        ]);
+
+        // Populate products table
+        DB::table('products')->insert([
+            [
+                "sku" => "000001",
+                "name" => "BV Lean leather ankle boots",
+                "category_id" => 1,
+                "created_at" => $current_date_time,
+                "updated_at" => $current_date_time
+            ],
+            [
+                "sku" => "000002",
+                "name" => "BV Lean leather ankle boots",
+                "category_id" => 1,
+                "created_at" => $current_date_time,
+                "updated_at" => $current_date_time
+            ],
+            [
+                "sku" => "000003",
+                "name" => "Ashlington leather ankle boots",
+                "category_id" => 1,
+                "created_at" => $current_date_time,
+                "updated_at" => $current_date_time
+            ],
+            [
+                "sku" => "000004",
+                "name" => "Naima embellished suede sandals",
+                "category_id" => 2,
+                "created_at" => $current_date_time,
+                "updated_at" => $current_date_time
+            ],
+            [
+                "sku" => "000005",
+                "name" => "Nathane leather sneakers",
+                "category_id" => 3,
+                "created_at" => $current_date_time,
+                "updated_at" => $current_date_time
+            ],
+        ]);
+
+        // Populate prices
+        $currency = 'EUR';
+        DB::table('prices')->insert([
+            [
+                "product_id" => 1,
+                "original_price" => 89000,
+                "currency" => $currency,
+                "created_at" => $current_date_time,
+                "updated_at" => $current_date_time
+            ],
+            [
+                "product_id" => 2,
+                "original_price" => 99000,
+                "currency" => $currency,
+                "created_at" => $current_date_time,
+                "updated_at" => $current_date_time
+            ],
+            [
+                "product_id" => 3,
+                "original_price" => 71000,
+                "currency" => $currency,
+                "created_at" => $current_date_time,
+                "updated_at" => $current_date_time
+            ],
+            [
+                "product_id" => 4,
+                "original_price" => 79500,
+                "currency" => $currency,
+                "created_at" => $current_date_time,
+                "updated_at" => $current_date_time
+            ],
+            [
+                "product_id" => 5,
+                "original_price" => 59000,
+                "currency" => $currency,
+                "created_at" => $current_date_time,
+                "updated_at" => $current_date_time
+
+            ],
+        ]);
+
+        // Populate discounts
+        DB::table('discounts')->insert([
+            [
+                "discount_percentage" => 30,
+                "discountable_type" => 'App\Models\Category',
+                "discountable_id" => 1,
+                "created_at" => $current_date_time,
+                "updated_at" => $current_date_time
+            ],
+            [
+                "discount_percentage" => 15,
+                "discountable_type" => 'App\Models\Product',
+                "discountable_id" => 3,
+                "created_at" => $current_date_time,
+                "updated_at" => $current_date_time
+            ]
+        ]);
+    }
+}
